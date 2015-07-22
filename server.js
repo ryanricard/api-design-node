32746ff7e7006c45a4841fa413cca17e294aacd9
Ryan Ricard <ryan.ricard@target.com>
2015-07-22 10:58:52 -0500

    Equivalent solution with `res.sendFile`

--- server.js
+++ server.js
@@ -3,17 +3,14 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
-
 var express = require('express');
-var fs = require('fs');
 var path = require('path');
 var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
 
 app.get('/', function(req, res) {
-  res.setHeader("content-type", "text/html");
-  fs.createReadStream('./index.html').pipe(res);
+  res.sendFile(path.resolve('index.html'));
 });
 
 app.get('/data', function(req, res) {
