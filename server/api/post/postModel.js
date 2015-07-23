60fac57358496c9728d46c68e44416e42d4050e6
Ryan Ricard <ryan.ricard@target.com>
2015-07-23 14:33:24 -0500

    Initial solution

--- server/api/post/postModel.js
+++ server/api/post/postModel.js
@@ -2,7 +2,25 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
 
+  text: {
+    type: String,
+    required: true
+  },
+
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+
+  categories: [{
+    type: mongoose.Schema.Types.ObjectId
+  }]
 });
 
 module.exports = mongoose.model('post', PostSchema);
